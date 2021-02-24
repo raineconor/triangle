02d42db02a7e4b04d2ffc034fb92151273c821c6
Raine Conor <raine.conor71@gmail.com>
2021-02-24 00:29:59 -0800

    update homepage

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 }
 
 body {
-  font-family:Arial, Helvetica, sans-serif;
+  font-family:Verdana, Arial, sans-serif;
   background-color:white;
   margin:0;
 }
@@ -63,7 +63,7 @@ textarea {
   min-height:auto;
   width:100%;
   position:relative;
-  padding:30px 15px 10px;
+  padding:15px;
   margin-right:auto;
   margin-left:auto;
   align-items:center;
@@ -72,24 +72,23 @@ textarea {
 #item1 {
   height:auto;
   min-height:auto;
-  width:160px;
+  width:140px;
   max-width:100%;
   display:inline-block;
   float:left;
 }
 
 #item2 {
-  background-color:rgb(71, 77, 227);
+  background-color:rgb(71, 139, 227);
   height:auto;
   width:auto;
   display:inline-block;
   position:absolute;
   padding:10px 30px;
-  margin-top:10px;
   right:15px;
   bottom:15px;
   border-radius:2px;
-  font-family:'Lato', sans-serif;
+  font-family:"Lato", sans-serif;
   font-size:16px;
   color:white;
   line-height:1;
@@ -101,7 +100,102 @@ textarea {
   background-color:#a0c3f0;
 }
 
-#item9, #item15, #item3 {
+#item3 {
+  background-color:white;
+  height:auto;
+  min-height:auto;
+  width:100%;
+  display:flex;
+  position:relative;
+  padding-right:15px;
+  padding-left:15px;
+  justify-content:center;
+  align-items:center;
+}
+
+#item4 {
+  height:auto;
+  min-height:80vh;
+  width:100%;
+  display:flex;
+  position:relative;
+  padding:40px 15px;
+  margin-right:auto;
+  margin-left:auto;
+  border-radius:2px;
+  flex-flow:row wrap;
+  justify-content:center;
+  align-items:center;
+}
+
+#item5 {
+  height:auto;
+  min-height:auto;
+  width:100%;
+  position:relative;
+  padding-top:15px;
+  padding-right:15px;
+  padding-bottom:15px;
+  text-align:left;
+}
+
+.heading {
+  background-color:inherit;
+  height:auto;
+  width:100%;
+  font-family:"Lato", sans-serif;
+  font-weight:700;
+  font-size:52px;
+  color:rgb(36, 45, 56);
+  line-height:1.15;
+  text-align:left;
+}
+
+#item7 {
+  height:auto;
+  width:100%;
+  margin-top:15px;
+  margin-bottom:15px;
+  font-family:"Lato", sans-serif;
+  font-weight:300;
+  font-size:22px;
+  color:rgb(36, 45, 56);
+  line-height:1.5;
+  text-align:left;
+}
+
+#item8 {
+  background-color:rgb(53, 206, 141);
+  height:auto;
+  width:auto;
+  display:inline-block;
+  padding:15px 50px;
+  margin-top:25px;
+  border-radius:2px;
+  font-family:"Lato", sans-serif;
+  font-size:16px;
+  color:white;
+  line-height:1;
+  text-align:center;
+  transition:all 120ms ease 0s;
+}
+
+#item8:hover {
+  background-color:#4f8edb;
+  color:white;
+}
+
+#item9 {
+  background-color:white;
+  height:auto;
+  min-height:auto;
+  width:100%;
+  margin-top:50px;
+  border-radius:3px;
+  box-shadow:gray 0px 0px 5px;
+}
+
+#item16, #item10 {
   background-color:white;
   height:auto;
   min-height:auto;
@@ -113,62 +207,7 @@ textarea {
   align-items:center;
 }
 
-#item4 {
-  background-color:rgb(191, 215, 234);
-  background-image:linear-gradient(to right bottom, rgb(185, 211, 232), rgb(233, 240, 251));
-  height:auto;
-  min-height:80vh;
-  width:100%;
-  display:flex;
-  position:relative;
-  padding:40px;
-  border-radius:2px;
-  flex-flow:row wrap;
-  justify-content:center;
-  align-items:center;
-}
-
-#item11, #item17, #item5 {
-  height:auto;
-  min-height:auto;
-  width:100%;
-  position:relative;
-  padding-top:15px;
-  padding-right:15px;
-  padding-bottom:15px;
-}
-
-#item12, #item18, #item24, #item6 {
-  background-color:inherit;
-  height:auto;
-  width:100%;
-  font-family:'Lato', sans-serif;
-  font-weight:bold;
-  font-size:52px;
-  color:rgb(36, 45, 56);
-  line-height:1;
-}
-
-#item13, #item19, #item7 {
-  background-color:inherit;
-  height:auto;
-  width:100%;
-  margin-top:25px;
-  margin-bottom:25px;
-  font-family:'Lato', sans-serif;
-  font-size:18px;
-  color:rgb(50, 63, 79);
-  line-height:1.5;
-}
-
-#item8 {
-  background-color:white;
-  height:auto;
-  min-height:1px;
-  width:100%;
-}
-
-#item10 {
+#item11 {
   background-color:rgb(191, 215, 234);
   background-image:linear-gradient(to right bottom, rgb(240, 205, 160), rgb(255, 241, 225));
   height:auto;
@@ -183,7 +222,30 @@ textarea {
   align-items:center;
 }
 
-#item14 {
+#item18, #item12 {
+  height:auto;
+  min-height:auto;
+  width:100%;
+  position:relative;
+  padding-top:15px;
+  padding-right:15px;
+  padding-bottom:15px;
+}
+
+#item20, #item14 {
+  background-color:inherit;
+  height:auto;
+  width:100%;
+  margin-top:25px;
+  margin-bottom:25px;
+  font-family:"Lato", sans-serif;
+  font-weight:300;
+  font-size:20px;
+  color:rgb(50, 63, 79);
+  line-height:1.5;
+}
+
+#item15 {
   height:auto;
   min-height:auto;
   width:100%;
@@ -191,7 +253,7 @@ textarea {
   display:inline-block;
 }
 
-#item16 {
+#item17 {
   background-color:rgb(191, 215, 234);
   background-image:linear-gradient(to right bottom, rgb(170, 240, 191), rgb(224, 249, 231));
   height:auto;
@@ -206,7 +268,7 @@ textarea {
   align-items:center;
 }
 
-#item20 {
+#item21 {
   background-color:rgb(29, 31, 33);
   height:auto;
   min-height:auto;
@@ -217,7 +279,7 @@ textarea {
   text-align:center;
 }
 
-#item21 {
+#item22 {
   height:auto;
   min-height:auto;
   width:100%;
@@ -226,7 +288,7 @@ textarea {
   border-right:5px solid rgb(41, 44, 47);
 }
 
-#item22 {
+#item23 {
   background-color:white;
   height:auto;
   min-height:auto;
@@ -239,7 +301,7 @@ textarea {
   align-items:center;
 }
 
-#item23 {
+#item24 {
   height:auto;
   min-height:auto;
   width:100%;
@@ -251,7 +313,7 @@ textarea {
   justify-content:center;
 }
 
-#item25 {
+#item26 {
   background-image:linear-gradient(to right bottom, rgb(33, 112, 214), rgb(71, 139, 227));
   height:auto;
   min-height:auto;
@@ -260,25 +322,25 @@ textarea {
   padding:10px;
 }
 
-#item26 {
+#item27 {
   height:auto;
   min-height:auto;
   width:100%;
   padding:15px;
-  font-family:'Lato', sans-serif;
+  font-family:"Lato", sans-serif;
   font-size:14px;
   color:white;
   line-height:1;
   text-align:center;
 }
 
-.banner-text {
-    font-size:36px;
+.heading {
+    font-size:42px;
 }
 
 @media(min-width:768px) {
-    .banner-text {
-        font-size:46px;
+    .heading {
+        font-size:52px;
     }
 }
 
@@ -316,37 +378,40 @@ textarea {
 
 
 @media (min-width: 768px) {
-  #item4, #item8, #item10, #item11, #item14, #item16, #item17, #item20, #item23, #item0 {
+  #item4, #item9, #item11, #item12, #item15, #item17, #item18, #item21, #item24, #item0 {
     width:100%;
   }
   #item5 {
     width:58.12%;
   }
-  #item21 {
+  #item22 {
     width:400px;
   }
 }
 
 @media (min-width: 992px) {
-  #item4, #item10, #item16, #item23, #item0 {
+  #item4, #item11, #item17, #item24, #item0 {
     width:100%;
   }
   #item1 {
-    width:160px;
+    width:140px;
   }
   #item5 {
     width:45%;
   }
-  #item8 {
+  #item9 {
     width:55%;
   }
-  #item14, #item17, #item20, #item11 {
+  #item15, #item18, #item21, #item12 {
     width:50%;
   }
 }
 
 @media (min-width: 1200px) {
-  #item4, #item10, #item16, #item23, #item0 {
+  #item4, #item11, #item17, #item0 {
+    width:1300px;
+  }
+  #item24 {
     width:1200px;
   }
 }
@@ -357,7 +422,7 @@ textarea {
 
 <!--========= Font Include: =========-->
 <link rel='preconnect' href='https://fonts.gstatic.com'>
-<link defer="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet" type="text/css">
+<link defer="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;500;700&amp;display=swap" triangle-font-family="Lato" rel="stylesheet" type="text/css">
 <!--=================================-->
 </head>
 
@@ -378,62 +443,67 @@ textarea {
     <div id="item3">
       <div id="item4">
         <div id="item5">
-          <div id="item6">
+          <div class="heading">
             Build Websites Fast<br>
           </div>
           <div id="item7">
-            Build beautiful, modern websites without spending time managing files.<br>
+            Build modern, elegant websites visually in your browser. Fast.<br>
+          </div>
+          <a href="register.php">
+            <div id="item8">
+              GET STARTED<br>
+            </div>
+            </a>
+          </div>
+          <div id="item9">
+            <video id="videoPlayer" src="https://trianglecms.com/video/triangle-preview-720p.mov" type="video/mp4" loop="" autoplay="true" width="100%" height="auto"></video>
           </div>
         </div>
-        <div id="item8">
-          <video id="videoPlayer" src="https://trianglecms.com/video/triangle-preview-720p.mov" type="video/mp4" loop="" autoplay="true" width="100%" height="auto"></video>
-        </div>
       </div>
-    </div>
-    <div id="item9">
       <div id="item10">
         <div id="item11">
           <div id="item12">
-            Enjoy Designing<br>
+            <div class="heading">
+              Made For Professionals<br>
+            </div>
+            <div id="item14">
+              From freelancer to agency, web design professionals use Triangle for rapid website design and development every day.<br>
+            </div>
           </div>
-          <div id="item13">
-            Triangle is so fun to use, you will wonder why you ever designed another way.<br>
+          <div id="item15">
+            <img src="images/triangle-home-background-2000.jpg" style="width: 100%; height: auto;">
           </div>
-        </div>
-        <div id="item14">
-          <img src="images/triangle-home-background-2000.jpg" style="width: 100%; height: auto;">
         </div>
       </div>
-    </div>
-    <div id="item15">
       <div id="item16">
         <div id="item17">
           <div id="item18">
-            Customize Everything<br>
+            <div class="heading">
+              Customize Everything<br>
+            </div>
+            <div id="item20">
+              Triangle is one of the only all-in-one online web design platforms that generates clean, editable code while you design visually.<br>
+            </div>
           </div>
-          <div id="item19">
-            Control every aspect of your websites, down to the code. <br>
-          </div>
-        </div>
-        <div id="item20">
           <div id="item21">
-            <img src="images/triangle-css.png" style="width: 100%; height: auto;">
+            <div id="item22">
+              <img src="images/triangle-css.png" style="width: 100%; height: auto;">
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div id="item22">
       <div id="item23">
         <div id="item24">
-          This website was made with Triangle.<br>
+          <div class="heading">
+            This website was made with Triangle.<br>
+          </div>
         </div>
       </div>
-    </div>
-    <div id="item25">
       <div id="item26">
-        © Copyright 2021 Raine Conor. All rights reserved. <br>
+        <div id="item27">
+          © Copyright 2021 Raine Conor. All rights reserved. <br>
+        </div>
       </div>
-    </div>
 
 </div><!-- end class="container" -->
 
@@ -445,12 +515,6 @@ function deferFonts() {
     defer[i].setAttribute("href", defer[i].getAttribute("defer"));
   }
 }
-
-</script>
-
-<script type="text/javascript">
-
-
 
 </script>
 
