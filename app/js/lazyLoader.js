9138fcb3d430e4c3e94dbece736c282831947c84
Raine Conor <raine.conor71@gmail.com>
2021-02-26 22:22:50 -0800

    fix global variable in for loop in lazyloader

--- app/js/lazyLoader.js
+++ app/js/lazyLoader.js
@@ -2,11 +2,11 @@ document.addEventListener("scroll", lazyload);
 function lazyload() {
   if (document.getElementById("myImages").style.display === "none") return;
   var lazyloadImg = document.querySelectorAll("img[lazyload]");
-  for (i = 0; i < lazyloadImg.length; i++) {
+  for (var i = 0; i < lazyloadImg.length; i++) {
     if (lazyloadImg[i].getBoundingClientRect().top < window.innerHeight + 300) {
       lazyloadImg[i].src = lazyloadImg[i].getAttribute("lazyload");
       lazyloadImg[i].removeAttribute("lazyload");
     }
-  }   
+  }
 }
 lazyload();
