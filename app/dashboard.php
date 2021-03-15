4ae419f173c0e4f1d3cbd27af56ad1e291ceb5fe
Raine Conor <raine.conor71@gmail.com>
2021-03-15 07:18:35 -0700

    use minified css/js

--- app/dashboard.php
+++ app/dashboard.php
@@ -29,7 +29,7 @@ if ($_SESSION["usertype"] === "admin") {
   <!--=========== CSS Include: =============-->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
   <link rel="stylesheet" href="dropzone/dropzone-dashboard.css" type="text/css" media="screen">
-  <link rel="stylesheet" href="dashboard-style.css" type="text/css" media="screen">
+  <link rel="stylesheet" href="dashboard-style.min.css" type="text/css" media="screen">
   <!--======================================-->
 
   <!--=========== Font Include: ============-->
--- app/index.php
+++ app/index.php
@@ -216,7 +216,7 @@ function lazyload() {
 <!-- <script type="text/javascript" src="iframeResizer/iframeResizer.min.js"></script>
 <script type="text/javascript" src="js/iframeResizer-init.min.js"></script> -->
 <script type="text/javascript" src="js/AJAX.min.js"></script>
-<script type="text/javascript" src="js/TRIANGLE.js"></script>
+<script type="text/javascript" src="js/TRIANGLE.min.js"></script>
 <script type="text/javascript" src="dropzone/dropzone.min.js"></script>
 <script type="text/javascript" src="dropzone/dropzone-init.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
